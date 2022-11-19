d315d1550dfa1bcc266ae5b2e2ceeba480d968aa
Sambit <63838832+Sambit-art@users.noreply.github.com>
2022-11-20 00:19:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import uvicorn
+app=FastAPI()
 
-app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+
+class InputPost(BaseModel):
+    title:str
+    body:str
+    userId:int
+    postId:int
+
+class outputPost(BaseModel):
+    title: str
+    body: str
+
+allpost=[
+    InputPost(title='first post title', body='first post body', userId=0,postId=1),
+    InputPost(title='second post title', body='second post body', userId=0,postId=2)]
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+async def home():
+    return {"hello":"world"}
 
+@app.post("/new",response_model=outputPost)
+async def addPost(post:InputPost):
+    allpost.append(post)
+    print(allpost)
+    return post
 
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+@app.get("/{postid}",response_model=outputPost)
+async def getPostById(postid:int):
+    for item in allpost:
+        if(item.postId==postid):
+            return item
+    return {item:item}
 
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+if __name__ == "__main__":
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
