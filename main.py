dbcdf5a09fb75c718d5202fc584eff430836af16
Sambit <63838832+Sambit-art@users.noreply.github.com>
2022-11-29 01:40:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,115 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+import pymongo
 import uvicorn
-app=FastAPI()
+from bson.objectid import ObjectId
+from fastapi import FastAPI
+from pydantic import BaseModel, json
 
+json.ENCODERS_BY_TYPE[ObjectId] = str
+app = FastAPI()
+
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+myclient = pymongo.MongoClient("mongodb+srv://sambit:sambit@demo.2isk9.mongodb.net/?retryWrites=true&w=majority")
+mydb = myclient["blogapp"]
+mycol = mydb["posts"]
+
+
+class Comment(BaseModel):
+    comment: str
+    userId: str
+    postId: str
 
 
 class InputPost(BaseModel):
-    title:str
-    body:str
-    userId:int
-    postId:int
+    title: str
+    body: str
+    userId: str
+
 
 class outputPost(BaseModel):
     title: str
     body: str
 
-allpost=[
-    InputPost(title='first post title', body='first post body', userId=0,postId=1),
-    InputPost(title='second post title', body='second post body', userId=0,postId=2)]
+
+class User(BaseModel):
+    username: str
+    password: str
 
 
-@app.get("/")
+allpost = []
+
+
+@app.get("/", tags=["posts"])
 async def home():
-    return {"hello":"world"}
+    result = myclient['blogapp']['posts'].aggregate([{'$addFields': {'kk': {'$toObjectId': '$userId'}}},
+        {'$lookup': {'from': 'users', 'localField': 'kk', 'foreignField': '_id', 'as': 'user'}},
+        {'$unwind': {'path': '$comment', 'includeArrayIndex': 'string'}},
+        {'$addFields': {'userget': {'$toObjectId': '$comment.userId'}}},
+        {'$lookup': {'from': 'users', 'localField': 'userget', 'foreignField': '_id', 'as': 'commentuser'}}, {
+            '$group': {'_id': '$_id', 'title': {'$first': '$title'}, 'body': {'$first': '$body'},
+                'user': {'$first': {'$first': '$user'}},
+                'comment': {'$push': {'comment': '$comment', 'user': {'$first': '$commentuser'}}}}}])
 
-@app.post("/new",response_model=outputPost)
-async def addPost(post:InputPost):
-    allpost.append(post)
-    print(allpost)
-    return post
+    # for doc in cur:
+    #     result.append(doc)
+    #     print(doc)
+    return list(result)
 
-@app.get("/{postid}",response_model=outputPost)
-async def getPostById(postid:int):
-    for item in allpost:
-        if(item.postId==postid):
-            return item
-    return {item:item}
+
+@app.post("/newPost", tags=["posts"])
+async def addPost(post: InputPost):
+    print(post.dict())
+    rec = {"title": post.dict()['title'], "body": post.dict()['body'], "userId": post.dict()['userId'], "comment": []}
+    print(rec)
+    mycol.insert_one(rec)
+    # print(post.json())
+    # print(allpost)
+    return {"status": True}
+
+
+# @app.get("/{postid}", tags=["posts"])
+# async def getPostById(postid: str):
+#     return {"user": [i for i in mycol.find({"_id": ObjectId(postid)})]}
+
+
+@app.post('/comment', tags=["comment", "posts"])
+async def insertComment(cm: Comment):
+    rec = {"comment": cm.dict()['comment'], "userId": cm.dict()['userId']}
+    mycol.update_one({"_id": ObjectId(cm.dict()['postId'])}, {"$push": {"comment": rec}})
+    print(rec)
+    return 0
+
+
+@app.post('/createuser', tags=['users'])
+async def createUser(user: User):
+    mydb['users'].insert_one(user.dict())
+    return {"status": True}
+
+
+@app.get("/getuserbyid", tags=['users'])
+async def getUser(id: str):
+    cur = mydb['users'].find({"_id": ObjectId(id)})
+    return list(cur)
+
+
+@app.put('/updateuserbyid', tags=['users'])
+async def updateuserbyid(id: str, user: User):
+    mydb['users'].update_one({"_id": ObjectId(id)}, {"$set": user.dict()})
+    return True
+
+
+@app.get('/getpostbyuser', tags=['users', 'posts'])
+async def getpostbyuser(userid: str):
+    cur = mydb['posts'].find({"userId": userid})
+    return list(cur)
+
+
+@app.delete('/deletecomment',tags=['comment'])
+async def deletecomment(postid: str, comment: str, userid: str):
+    cur = mydb['posts'].update_one({"_id": ObjectId(postid)},
+        {"$pull": {"comment": {"$and": [{"comment": comment}, {"userId": userid}]}}})
+    return {"status": True}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
